041ebe255088eef296cf9a04da4d1e0f48c6c856
joseabrahammartinezz <89718616+joseabrahammartinezz@users.noreply.github.com>
2023-04-20 18:57:22 -0400

    Update views.py

--- Api/apis/views.py
+++ Api/apis/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from Api.models import Gestor_Banco, Pedido_Materiais
-from Api.apis.serializers import GestorBancoSerializer, PedidoMateriaisSerializer
+from Api.models import Gestor_Banco, Pedido_Materiais, VIEW_VENTAS, VIEW_MASTER, VIEW_DETAIL
+from Api.apis.serializers import GestorBancoSerializer, PedidoMateriaisSerializer, ViewVentasSerializer, DatosFacturaSerializer, ViewMasterSerializer, ViewDetailSerializer
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -18,7 +18,7 @@ class PedidoMateriaisApiViewSet(ModelViewSet):
 @csrf_exempt
 def PedidoMateriaisApi(request,id=0):
     if request.method=='GET':
-        pedidos = Pedido_Materiais.objects.all()
+        pedidos = Pedido_Materiais.objects.all()[:10]
         pedidos_serializer = PedidoMateriaisSerializer(pedidos, many=True)
         return JsonResponse(pedidos_serializer.data, safe=False)
     elif request.method=='POST':
@@ -40,4 +40,36 @@ def PedidoMateriaisApi(request,id=0):
         pedido=Pedido_Materiais.objects.get(PEDIDO=id)
         pedido.delete()
         return JsonResponse("Eliminacion exitosa", safe=False)
+    
+@csrf_exempt
+def PedidoMateriaisBusquedaApi(request, numpedido, empresa):
+    if request.method=='GET':
+        print (numpedido)
+        print ( empresa)
+        pedidos = Pedido_Materiais.objects.filter(PEDIDO=numpedido, EMPRESA=empresa)[:10]
+        pedidos_serializer = PedidoMateriaisSerializer(pedidos, many=True)
+        return JsonResponse(pedidos_serializer.data, safe=False)
+    
+@csrf_exempt
+def ViewVentasApi(request,id=0):
+    if request.method=='GET':
+        pedidos = VIEW_VENTAS.objects.all()[:10]
+        pedidos_serializer = ViewVentasSerializer(pedidos, many=True)
+        return JsonResponse(pedidos_serializer.data, safe=False)
+    
+@csrf_exempt
+def ViewVentasBusqueddaApi(request, numpedido, empresa):
+    if request.method=='GET':
+        print (numpedido)
+        print ( empresa)
+        master_info = VIEW_MASTER.objects.filter(PEDIDO=numpedido, EMPRESA=empresa)[:10]
+
+        detail_info = VIEW_DETAIL.objects.filter(PEDIDO=numpedido, EMPRESA=empresa)[:10]
+
+        return JsonResponse(
+            DatosFacturaSerializer({
+                "cabecera" : master_info,
+                "detalle" : detail_info,
+            }).data, safe = False, status=200,
+        )
     
